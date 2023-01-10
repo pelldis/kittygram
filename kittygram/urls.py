243a70bf8a8486a73b56da369855cad6138db877
Anton Kozlov <anton-kozlov@yandex-team.ru>
2023-01-10 19:51:57 +0300

    example with DefaultRouter

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import SimpleRouter, DefaultRouter
 from cats.views import CatViewSet
 
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register('cats', CatViewSet)
 
 urlpatterns = [
