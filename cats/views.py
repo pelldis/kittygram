2ef85f5a3c3f1cd8211676fb9da2d97f5137ee42
Anton Kozlov <anton-kozlov@yandex-team.ru>
2023-01-10 06:15:29 +0300

    view class

--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,4 @@
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -6,14 +7,46 @@ from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
         serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+
+
+class APICatDetail(APIView):
+    def get(self, request, pk):
+        cat = Cat.objects.get(id=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        cat = Cat.objects.get(id=pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+        return Response(serializer.errors, status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, pk):
+        cat = Cat.objects.get(id=pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+        return Response(serializer.errors, status.HTTP_204_NO_CONTENT)
+
+    def delete(self, request, pk):
+        try:
+            cat = Cat.objects.get(id=pk)
+            cat.delete()
+            return Response('', status.HTTP_204_NO_CONTENT)
+        except Exception as error:
+            return Response(f'{error}', status.HTTP_404_NOT_FOUND)
