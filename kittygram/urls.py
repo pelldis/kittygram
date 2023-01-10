e0ccd62acfb566324badae1972d9141238d2dcc7
Anton Kozlov <anton-kozlov@yandex-team.ru>
2023-01-10 19:27:44 +0300

    viewset example

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+from cats.views import CatViewSet
 
-from cats.views import CatList, CatDetail
 
+router = SimpleRouter()
+router.register('cats', CatViewSet)
 
 urlpatterns = [
-   path('cats/', CatList.as_view()),
-   path('cats/<int:pk>/', CatDetail.as_view())
+   path('', include(router.urls)),
 ]
